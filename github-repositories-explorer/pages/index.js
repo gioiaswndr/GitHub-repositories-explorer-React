7af3ed74ff74b37be9347b2056f6c643ff450afe
gioiaswndr <annashkagioia24@gmail.com>
2023-01-27 05:02:22 +0700

    fixing text responsive issue

--- github-repositories-explorer/pages/index.js
+++ github-repositories-explorer/pages/index.js
@@ -33,48 +33,78 @@ export default function Home() {
           <Accordion.Body className='bg-danger my-2'>
             <div className='d-flex justify-content-around'>
               <div>
-                <p className='fs-5'>REPO TITLE</p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-              </div>
-              <div className='bg-primary w-50 text-center'>
-                <p className='m-auto w-25'>
-                  12 X
+                <p className='fs-5 fw-bold'>REPO TITLE</p>
+                <p className='text-start'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+              </div>
+              <div className='bg-primary w-25 m-1'>
+                <p className='text-nowrap text-center'>12 X</p>
               </div>
             </div>
           </Accordion.Body>
           <Accordion.Body className='bg-danger my-2'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <div className='d-flex justify-content-around'>
+              <div>
+                <p className='fs-5 fw-bold'>REPO TITLE</p>
+                <p className='text-start'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+              <div className='bg-primary w-25 m-1'>
+                <p className='text-nowrap text-center'>12 X</p>
+              </div>
+            </div>
           </Accordion.Body>
           <Accordion.Body className='bg-danger my-2'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <div className='d-flex justify-content-around'>
+              <div>
+                <p className='fs-5 fw-bold'>REPO TITLE</p>
+                <p className='text-start'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+              <div className='bg-primary w-25 m-1'>
+                <p className='text-nowrap text-center'>12 X</p>
+              </div>
+            </div>
           </Accordion.Body>
           <Accordion.Body className='bg-danger my-2'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <div className='d-flex justify-content-around'>
+              <div>
+                <p className='fs-5 fw-bold'>REPO TITLE</p>
+                <p className='text-start'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+              <div className='bg-primary w-25 m-1'>
+                <p className='text-nowrap text-center'>12 X</p>
+              </div>
+            </div>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1" className='border-0'>
